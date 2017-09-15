4995dd6d3e6635ebdadd57646688a4bb3995c2b2
BernaidleQuadcopter <woody.willman2@gmial.com>
2017-09-15 12:08:46 +1000

    Added player and terrain

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    public float speed = 50f;
+
+    private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift) ||
+            Input.GetKeyDown(KeyCode.Space))
+        
+        {
+            Vector3 dashDir = transform.forward;
+            rigid.AddForce(dashDir * speed, ForceMode.Impulse);
+
+        }
+		
+	}
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float Speed = 5f;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0, inputH * Speed, 0));
+
+	}
+}
